2d01256ac2930443967f8b192e46bcd35fd588fc
dxd110 <77091129+dxd110@users.noreply.github.com>
2023-09-16 17:31:59 +0800

    Update momo_random.js

--- momo_random.js
+++ momo_random.js
@@ -3,7 +3,7 @@
  */
  const $ = new Env("墨墨背单词share_random");
  const notify = $.isNode() ? require('./sendNotify') : '';
- const Notify = 1; //0为关闭通知，1为打开通知,默认为1
+ const Notify = 0; //0为关闭通知，1为打开通知,默认为1
  const debug = 1; //0为关闭调试，1为打开调试,默认为0
 
  let msg = '';
