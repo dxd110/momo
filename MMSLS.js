ca78253685d476600760af0957f458a8bca2129a
dxd110 <77091129+dxd110@users.noreply.github.com>
2023-10-23 23:23:41 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=24921597&pid=4ef7054c9e2a4b9bca8395455ec95d16&tid=ad27bccbe9b32656cc94c602e4d57774",
+    "https://www.maimemo.com/share/page?uid=24921597&pid=5104099a3f33caa9d125da8a2e8f10ba&tid=62e52e6b07ef7b3f33071ab700fe5910",
   ]
   /**
    * 生成随机数字
