f993910333ee2d443ba537ddae18b0e261a55b6b
dxd110 <77091129+dxd110@users.noreply.github.com>
2025-05-10 21:57:11 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,9 +6,9 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=24921597&pid=75c4948ded804df4a33dad40ac3d4c22&tid=cce6c7827d78cfe277c4104dfff3cc8c,
-    "https://www.maimemo.com/share/page?uid=24921597&pid=75c4948ded804df4a33dad40ac3d4c22&tid=cce6c7827d78cfe277c4104dfff3cc8c,
-    "https://www.maimemo.com/share/page?uid=24921597&pid=75c4948ded804df4a33dad40ac3d4c22&tid=cce6c7827d78cfe277c4104dfff3cc8c,
+    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
+    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
+    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
   ]
 =====================以上为范例！=====================
 
